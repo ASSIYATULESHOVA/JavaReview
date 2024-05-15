f8698a1b2e63b0d9e847e883ff1426470f2b9df7
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-05-15 18:32:32 -0400

    Java Review

---
+++ src/week02_Review/practiceTasks/SwapToVariables.java
@@ -0,0 +1,23 @@
+package week02_Review.practiceTasks;
+
+public class SwapToVariables {
+
+    public static void main(String[] args) {
+
+
+        int a = 1000;
+        int b = 1500;
+
+        a =  a + b;  // a = 25
+        b = a - b; // b = 10
+        a = a - b;  // a = 15
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+
+
+    }
+
+}
---
+++ src/week02_Review/practiceTasks/Variables.java
@@ -0,0 +1,42 @@
+package week02_Review.practiceTasks;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        // DataType variable = Data;
+
+        int num = 100;
+
+        double num2 = 2.5;
+
+        char ch = 40000;
+
+        boolean r = 10 > 0;
+
+        num = 200;
+
+        System.out.println(num);
+        System.out.println(num2);
+        System.out.println(ch);
+        System.out.println(r);
+
+        long l = 99999999999L;
+
+
+        String name = "Java";
+
+        System.out.println(name);
+
+        byte a = 100;
+
+        int b = a;
+
+        long c = 300;
+
+        short d = (short) c;
+
+
+
+    }
+}
