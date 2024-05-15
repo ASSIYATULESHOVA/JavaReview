ccfdff2e484a25161e061ffe09f37983182cd4af
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-05-15 18:30:50 -0400

    Java Review

---
+++ src/week02_Review/practiceTasks/EscapeSequences.java
@@ -0,0 +1,23 @@
+package week02_Review.practiceTasks;
+
+public class EscapeSequences {
+
+    public static void main(String[] args) {
+
+        /*
+            \t
+            \n
+            \"
+         */
+
+
+        System.out.println("\tJava Developer\n\tBatch02");
+
+        System.out.println("Today is \"Sunday\"");
+
+
+
+    }
+
+
+}
---
+++ src/week02_Review/practiceTasks/PrintStatementWithMainMethod.java
@@ -0,0 +1,19 @@
+package week02_Review.practiceTasks;
+
+public class PrintStatementWithMainMethod {
+
+    public static void main(String[] args) {
+
+        // System.out.print("Java Programming Language");  // Prints "Java Programming", does not append a new line
+        System.out.println("Java Programming Language");  // Prints "Java Programming", appends a new line
+        /*
+        System.out.println();
+       System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+         */
+
+    }
+}
