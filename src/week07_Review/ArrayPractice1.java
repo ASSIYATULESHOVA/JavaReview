4824794af501f4498132a809de45fd1335c9a426
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-05-20 18:33:24 -0400

    Java Review 7

---
+++ src/week07_Review/ArrayPractice1.java
@@ -0,0 +1,27 @@
+package week07_Review;
+
+import week06_Review.practice_tasks.Pizza;
+
+public class ArrayPractice1 {
+
+    public static void main(String[] args) {
+
+        Pizza pizza1 = new Pizza();
+        pizza1.setInfo("small", 2, 3,4);
+
+        Pizza pizza2 = new Pizza();
+        pizza2.setInfo("medium", 3, 4, 3);
+
+        Pizza pizza3 = new Pizza();
+        pizza3.setInfo("large", 4, 5, 5);
+
+
+        pizza1.numberOfCheeseTopping = 10;
+
+        System.out.println(pizza1);
+        System.out.println(pizza2);
+        System.out.println(pizza3);
+
+    }
+
+}
