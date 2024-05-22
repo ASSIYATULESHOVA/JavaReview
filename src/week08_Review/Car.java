47e7a94e7b33c368c79b346fb772b1d18097733d
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-05-22 15:01:53 -0400

    Java Review 8

---
+++ src/week08_Review/Car.java
@@ -0,0 +1,20 @@
+package week08_Review;
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+public class Car {
+
+    private String make;
+    private String model;
+    private int year;
+    private String color;
+    private double price;
+
+
+
+}
---
+++ src/week08_Review/practice_tasks/TestCarObject.java
@@ -0,0 +1,15 @@
+package week08_Review.practice_tasks;
+
+import week08_Review.Car;
+
+public class TestCarObject {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+
+
+
+
+    }
+}
