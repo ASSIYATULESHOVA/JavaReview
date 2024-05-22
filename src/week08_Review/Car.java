12ecddc419f899866526a13a6b1748bfbc642227
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-05-22 15:06:18 -0400

    Java Review 8

--- src/week08_Review/Car.java
+++ src/week08_Review/Car.java
@@ -17,15 +17,54 @@ public class Car {
     }
 
 
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
     @Override
     public String toString() {
         return "Car{" +
-                "make='" + make + '\'' +
-                ", model='" + model + '\'' +
-                ", year=" + year +
-                ", color='" + color + '\'' +
-                ", price=" + price +
+                "make = " + make + '\'' +
+                ", model = " + model + '\'' +
+                ", year = " + year +
+                ", color = " + color + '\'' +
+                ", price = " + price +
                 '}';
     }
 }
