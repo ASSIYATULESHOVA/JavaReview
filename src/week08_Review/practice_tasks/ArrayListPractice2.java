5a1ed63aa12e2427ce3d4ede2acc85d93f164ad2
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-05-23 19:08:49 -0400

    Java Review 8

---
+++ src/week08_Review/practice_tasks/ArrayListPractice2.java
@@ -0,0 +1,25 @@
+package week08_Review.practice_tasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListPractice2 {
+
+    public static void main(String[] args) {
+
+
+
+            ArrayList<String> list = new ArrayList<>();
+            list.addAll(Arrays.asList("Anna", "Canada", "Bob", "David", "Lan", "Abida", "Ebrahim", "Farida"));
+
+            //Predicate<String> sameFirstAndLastChar = p -> p.toLowerCase().charAt(0) == p.toLowerCase().charAt(p.length()-1);
+
+            // list.removeIf(sameFirstAndLastChar);
+            list.removeIf(p -> p.toLowerCase().charAt(0) == p.toLowerCase().charAt(p.length()-1));
+
+            System.out.println(list);
+
+
+    }
+
+}
