15ee0674daf788c61e83cc4b7ee719d5df81df3f
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-05-23 19:13:12 -0400

    Java Review 7

---
+++ src/week07_Review/practice_tasks/Item.java
@@ -0,0 +1,29 @@
+package week07_Review.practice_tasks;
+
+public class Item {
+
+    public String itemName;
+    public int quantity;
+    public double unitPrice;
+
+    public Item(String itemName, int quantity, double unitPrice){
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    public double calcCost(){
+        return  unitPrice * quantity;
+    }
+
+
+    public String toString() { // Jason
+        return "Item{" +
+                "itemName='" + itemName + '\'' +
+                ", quantity=" + quantity +
+                ", unitPrice=" + unitPrice +
+                ", totalPrice=" + calcCost() +
+                '}';
+    }
+
+}
