99dbe62b03eaee176687d2d39d7c124448313515
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-05-24 16:28:40 -0400

    Java Review 8

---
+++ src/week09_Review/Cloth.java
@@ -0,0 +1,19 @@
+package week09_Review;
+
+public class Cloth {
+
+    private String type;
+    private String color;
+    private String material;
+    private char size;
+    private int quantity;
+
+
+    public Cloth(String type, String color, String material, char size, int quantity) {
+        this.type = type;
+        this.color = color;
+        this.material = material;
+        this.size = size;
+        this.quantity = quantity;
+    }
+}
