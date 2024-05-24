e4780a51c63095eb04144be7a2ff757766681222
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-05-24 16:31:57 -0400

    Java Review 8

---
+++ src/week09_Review/TestClothObjects.java
@@ -0,0 +1,10 @@
+package week09_Review;
+
+public class TestClothObjects {
+
+    public static void main(String[] args) {
+
+        Cloth cloth1 = new Cloth("Dress","White","Cotton",'M',15);
+
+    }
+}
