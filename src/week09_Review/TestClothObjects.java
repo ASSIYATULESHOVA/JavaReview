f235ff4a1927df4fcfada96589de04ccff3c9b22
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-05-28 22:16:58 -0400

    Java Review 8

--- src/week09_Review/TestClothObjects.java
+++ src/week09_Review/TestClothObjects.java
@@ -7,5 +7,13 @@ public class TestClothObjects {
         Cloth cloth1 = new Cloth("Dress","White","Cotton",'M',15);
 
         Cloth cloth2 = new Cloth("Shirt","Blue","Cotton",'M',16);
+
+        Cloth cloth3 = new Cloth("Pants","Black","Polyester",'L',20);
+
+
+        System.out.println(cloth1);
+        System.out.println(cloth2);
+
+
     }
 }
