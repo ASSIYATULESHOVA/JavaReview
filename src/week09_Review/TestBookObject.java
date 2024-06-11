129ff273516dda95fc93e596c92c4d982b6b570a
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-06-11 17:18:57 -0400

    Java Review 8

--- src/week09_Review/TestBookObject.java
+++ src/week09_Review/TestBookObject.java
@@ -5,7 +5,7 @@ public class TestBookObject {
 
         Book book1 = new Book("War and Piece","Lev Tolstoy",1956,"Criminal");
 
-        Book book2 = new Book("Hunter","Joe Belousov",1999);
+        Book book2 = new Book("Hunter","Joe Be",1999);
 
         Book book3 = new Book("Api","Amazon",2024);
         Book book4 = new Book("Api","Amazon",2024);
