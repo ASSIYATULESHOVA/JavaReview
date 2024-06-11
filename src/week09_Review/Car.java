7ac8585a5db08e9cf3b115471c8d7cb69027f442
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-06-11 17:21:08 -0400

    Java Review 8

--- src/week09_Review/Car.java
+++ src/week09_Review/Car.java
@@ -7,4 +7,54 @@ public class Car {
     private short year;
     private double price;
 
+
+    public Car(String made, String model, String color, short year, double price) {
+        this.made = made;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.price = price;
+    }
+
+
+
+    public String getMade() {
+        return made;
+    }
+
+    public void setMade(String made) {
+        this.made = made;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public short getYear() {
+        return year;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
