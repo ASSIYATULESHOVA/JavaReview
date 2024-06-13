e924e92afe180db3b6b8d517ea9fd370e12ce5a0
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-06-12 20:45:12 -0400

    Java Review 8

--- src/week09_Review/TestCarObject.java
+++ src/week09_Review/TestCarObject.java
@@ -6,5 +6,8 @@ public class TestCarObject {
 
         Car car1 = new Car("BMW","X6","White",2022,65000);
         System.out.println(car1);
+
+        Car car2 = new Car("Honda","Accord","Black",2019,30000.00);
+        System.out.println(car2);
     }
 }
