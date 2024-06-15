7f26fc92fc9832def7590f1f19b59c2ca681bf4e
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-06-15 18:49:54 -0400

    Java Review 8

--- src/week09_Review/TestBookObject.java
+++ src/week09_Review/TestBookObject.java
@@ -8,11 +8,13 @@ public class TestBookObject {
         Book book3 = new Book("Api","Amazon",2024);
         Book book4 = new Book("Secret","Amazon",1999);
         Book book5 = new Book("Interview","Jack Paul",2020);
+        Book book6 = new Book("How to raise a Boy?","BJ Olsen",2002);
 
         System.out.println(book1);
         System.out.println(book2);
         System.out.println(book3);
         System.out.println(book4);
+        System.out.println(book5);
 
 
 
